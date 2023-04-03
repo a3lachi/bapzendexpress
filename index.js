c6d98158e2a3b521b390a91383a6e4d651964161
a3laxi <mad.aalachi@gmail.com>
2023-04-03 09:34:50 +0200

    yesop

--- index.js
+++ index.js
@@ -10,71 +10,66 @@ const path = require ('path')
 
 
 
-const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const swaggerJSDoc = require('swagger-jsdoc');
 
-
-// // const YAML = require('yamljs');
-// // const swaggerDocument = YAML.load('./swagger.yaml');
 
 
 // //--------------------------------------------------------------------------------------------------------------------------------
 
 
 const app = express();
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cors()); // Allow all origins to access the API
-// app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors()); // Allow all origins to access the API
+app.use(express.static('public'));
 
 
-// // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// //--------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------
 
 
 
 // ///////////////////////////////////////////////////////////////
-// const imagePath = './images/';
+const imagePath = './images/';
+
+//////////////
+const getSrc = (name) => {
+  try {
+    const files = fs.readdirSync(imagePath)  
+    const srcs = []
+    for (const image of files) {
+      imgname = image.split('.jpg')[0].slice(0,-1)
+      if (imgname === name ) {
+        srcs.push(image)
+      }
+    }
+    return srcs 
+  } catch(error) {
+    return [error]
+  }
+}
+//////////////
+
 
 // //////////////
-// const getSrc = (name) => {
-//   try {
-//     const files = fs.readdirSync(imagePath)  
-//     const srcs = []
-//     for (const image of files) {
-//       imgname = image.split('.jpg')[0].slice(0,-1)
-//       if (imgname === name ) {
-//         srcs.push(image)
-//       }
-//     }
-//     return srcs 
-//   } catch(error) {
-//     return [error]
-//   }
-// }
-// //////////////
-
-
-// //////////////
-// const araJSON = (bigint) => {
-//   console.log('))))))',bigint)
-//   return(
-//     {
-//       'id':Number(bigint.id.toString()),
-//       'productname':bigint.productname,
-//       'price':bigint.price,
-//       'color':bigint.color,
-//       'category':bigint.category,
-//       'size':bigint.size,
-//     }
-//   )
-// }
+const araJSON = (bigint) => {
+  console.log('))))))',bigint)
+  return(
+    {
+      'id':Number(bigint.id.toString()),
+      'productname':bigint.productname,
+      'price':bigint.price,
+      'color':bigint.color,
+      'category':bigint.category,
+      'size':bigint.size,
+    }
+  )
+}
 
 // //////////////
 
@@ -89,72 +84,49 @@ const app = express();
 
 
 // //    / GET  ///////////////////////////////////////////////////
-// app.get('/', (req, res) => {
-//   res.send({'info':"API CORRECTLY WORKING."})
-// });
-// ///////////////////////////////////////////////////////////////
+app.get('/', (req, res) => {
+  res.send({'info':"API CORRECTLY WORKING."})
+});
+///////////////////////////////////////////////////////////////
 
-// //    / GET  ///////////////////////////////////////////////////
-// app.get('/images', (req, res) => {
+//    / GET  ///////////////////////////////////////////////////
+app.get('/images', (req, res) => {
 
-//   const file = path.join(process.cwd(), './', 'data.txt');
-//   const stringified = fs.readFileSync(file, 'utf8');
+  const file = path.join(process.cwd(), './', 'data.txt');
+  const stringified = fs.readFileSync(file, 'utf8');
 
 
-//   res.send({'data':stringified})
-// });
+  res.send({'data':stringified})
+});
 // ///////////////////////////////////////////////////////////////
 
 
 
 
 // //////   POST  /////////////////////////////////////////////////
-// app.post('/api/bapz/id', async (req, res) => {
+app.post('/api/bapz/id', async (req, res) => {
 
-//   // get elements from database
-//   const product = await prisma.bapz.findMany({
-//     where: {
-//       id: BigInt(req.body.id),
-//     }
-//   });
+  // get elements from database
+  const product = await prisma.bapz.findMany({
+    where: {
+      id: BigInt(req.body.id),
+    }
+  });
 
-//   // deal with element
-//   if (product.length === 1) {
-//     const name = product[0].productname.split(' ').join('')
-//     const srcs = await getSrc(name)
-//     res.send({'found':"yes",'src': srcs , 'data':araJSON(product[0])})
-//   }
-//   // product with id = ID don't exist in database
-//   else 
-//     res.send({'data': 'not found'})
-// });
+  // deal with element
+  if (product.length === 1) {
+    const name = product[0].productname.split(' ').join('')
+    const srcs = await getSrc(name)
+    res.send({'found':"yes",'src': srcs , 'data':araJSON(product[0])})
+  }
+  // product with id = ID don't exist in database
+  else 
+    res.send({'data': 'not found'})
+});
 // ///////////////////////////////////////////////////////////////
 
 
 // //--------------------------------------------------------------------------------------------------------------------------------
-
-
-// const yaml = require('js-yaml');
-
-// const swaggerPath = path.join(__dirname, 'swagger.yaml');
-// const swaggerFileContents = fs.readFileSync(swaggerPath, 'utf8');
-// const swaggerDocument = yaml.load(swaggerFileContents);
-
-
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerJsdoc = require('swagger-jsdoc');
-
-// const options = {
-//     swaggerDefinition: swaggerDocument,
-//     apis: []
-// };
-
-// const specs = swaggerJsdoc(options);
-
-// app.use('/api-docs', swaggerUi.serve);
-// app.get('/api-docs', swaggerUi.setup(specs));
-
-
 
 
 
@@ -192,13 +164,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOp
 // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// Define your routes
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 // Start the server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
